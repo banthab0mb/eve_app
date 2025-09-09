24640cecdb9547826050ee4412760b4e798d245f
banthab0mb <hrudowski0882@gmail.com>
2025-09-08 22:01:46 -0400

    more gitignore

--- e.js
+++
@@ -1,8 +0,0 @@
-const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database("systems.db");
-
-db.all("SELECT COUNT(*) AS count FROM systems", [], (err, rows) => {
-  if (err) throw err;
-  console.log("Systems table row count:", rows[0].count);
-  db.close();
-});
